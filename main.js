7877a6e80fccaa4855c7aa46ca94a8fe8ef0ea2f
Kevin Konradi <kevin.konradi@icloud.com>
2020-12-11 23:19:05 +0100

    yeetus deletus changus

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("./config.json");
+const config = require("./config/config.json");
 const client = new Discord.Client();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const prefix = "!";
